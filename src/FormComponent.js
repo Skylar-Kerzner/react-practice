87c3b3bf11668e3dc570ab6c53a1a5a13ade51bb
Skylar Kerzner <skerzner@community.com>
2020-08-18 00:18:52 -0700

    3 photos with description

--- src/FormComponent.js
+++ src/FormComponent.js
@@ -22,18 +22,41 @@ class FormComponent extends Component {
         const response = await fetch(url)
         const data = await response.json()
     }
-
+    /*
     thenGetImage(url) {
         const response = fetch(url)
         .then(response => response.json())
-        .then(data => data.errors == "No photos found." ?
-            $( 'div.image_holder' ).append('No related photos found. <br><br>')  :
+        .then(data => 
+            if (data.errors == "No photos found.") {
+                $( 'div.image_holder' ).append('No related photos found. <br><br>')
+            } else {
             this.setState({
             image_url: data.urls.small,
             html_link: data.links.html,
             isImageReady: true,
             })
         ) 
+    }
+*/
+    appendGetImage(url) {
+        const response = fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data.errors == "No photos found.") {
+                $( 'div.image_holder' ).append('No related photos found. <br><br>')
+            } else {
+                $( 'div.image_holder' ).append('<img className="App-logo" src='+data.urls.small+' />')
+                $( 'div.link_holder' ).append(data.links.html)
+                $( 'div.description_holder' ).empty()
+                if (data.user.location) {
+                    $( 'div.description_holder' ).append('The photographer is from '+data.user.location+'. ')
+                }
+                if (data.location.name) {
+                    $( 'div.description_holder' ).append('The photo was taken in '+data.location.name+'.')
+            }
+            }
+        }
+        )
     }
    
 
@@ -42,10 +65,12 @@ class FormComponent extends Component {
         console.log(this.state)
         $( 'h3' ).remove()
         $ ('#form1').remove()
-        $( 'div.submit_holder' ).append('<h3> Search submitted. </h3> <br><br>')
+        $( 'div.description_holder' ).append('<h3> Submitted </h3> <br><br>')
         const url = 'https://api.unsplash.com/photos/random?' + 'client_id=' + this.state.unsplashApiKey + '&query=' + this.state.field1
-        $( 'div.submit_holder' )//.append(url+'<br><br>')
-        this.thenGetImage(url)
+        //$( 'div.submit_holder' ).append(url+'<br><br>')
+        this.appendGetImage(url)
+        this.appendGetImage(url)
+        this.appendGetImage(url)
     }
 
     render() {
@@ -75,11 +100,12 @@ class FormComponent extends Component {
                 <div class="debug_holder">
                     {isImageReady ? <h6>{this.state.image_url}</h6> : <h6>debug will go here</h6>} 
                 </div> */}
-                <div class="submit_holder"></div>
+    
                 
                 <div class="image_holder"> 
-                    {isImageReady ? <img src={this.state.image_url} /> : <h6></h6>} 
+                    {isImageReady ? <img src={this.state.image_url} /> : <h1></h1>} 
                 </div>
+                <div class="description_holder"><br></br></div>
                 <div class="link_holder invisible">
                     {isImageReady ? <h6>{this.state.html_link}</h6> : <h6></h6>} 
                 </div>
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class MyComponent extends Component {
 
     state = {
-            'myname' : 'Skylar Grant Kerzner',
+            'myname' : 'Skylar Kerzner',
             'myage' : '28'
         }
 
@@ -15,9 +15,7 @@ class MyComponent extends Component {
                     <em>{this.props.greeting}{this.props.audience}.</em>
                 </h2> 
                 <p class='paragraph'>
-                    My name is {this.state.myname} <br></br>
-                    I am {this.state.myage} years old <br></br>
-                    And this is my React component. 
+                    Created by {this.state.myname} <br></br>
                 </p>
                 
             </div>
